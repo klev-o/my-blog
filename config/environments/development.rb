83c3b238ae6307896bac113b062efef9819a4843
Konstantin <x-stels@bk.ru>
2017-10-23 22:13:07 +0300

    Загатовка под стартовую страничку + сменен дефолтный роут

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.generators do |g|
+    g.orm :active_record
+    g.template_engine :slim
+  end
+
 end
