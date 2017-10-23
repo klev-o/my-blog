626d56b224d9f68469bc25deede01545e3895e57
Konstantin <x-stels@bk.ru>
2017-10-24 01:47:04 +0300

    Бутстрап реально бета...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery_ujs
+//= require jquery
 //= require turbolinks
-//= require_tree .
-//= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
+//= require_tree .
+
