30a06dab07caf9527d9ca2d3a7279f761c1f0675
Konstantin <x-stels@bk.ru>
2017-10-24 02:17:30 +0300

    Что-то вроде футера

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :year, :author
+
+  protected
+
+  def year
+    2017
+  end
+
+  def author
+    'Konstantin'
+  end
+
 end
