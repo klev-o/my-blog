7dc5c5d27837fec2c5bf1a92e58eb4819f7f098a
Levachin Konstantin <k.levachin@newcontact.su>
2017-10-25 18:34:06 +0300

    Реализовано несколько страниц

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'about/index'
 
   resources :about
+  resources :main
+  resources :gallery
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
