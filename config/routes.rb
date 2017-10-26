15bbca72bcf6e4f5b6860f3e844efb4211ae719a
Igor Simdyanov <igorsimdyanov@gmail.com>
2017-10-26 15:43:35 +0300

    Fixes for lesson 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   get 'main/index'
   get 'about/index'
 
-  resources :about
-  resources :main
-  resources :gallery
-
+  resources :about, only: :index
+  resources :main, only: :index
+  resources :gallery, only: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'main#index'
+  root to: 'main#index'
 end
