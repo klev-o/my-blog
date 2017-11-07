ffe84e4a6718df04f5c8356bd5f4cb374ed5aef8
Levachin Konstantin <k.levachin@newcontact.su>
2017-11-07 14:57:31 +0300

    Добавлены миграции, прописана некоторая валидация для моделей

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  has_many :commentaries, :dependent => :destroy
+
   validates :title, presence: true
   validates :title, length: { maximum: 1000, minimum: 5 }
   validates :body, presence: true
+
+  after_create :put_to_log
+
+  private
+
+  def put_to_log
+    Rails.logger.info '#########################################'
+    Rails.logger.info 'Hello from callback'
+    Rails.logger.info '#########################################'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  has_many :commentaries
+  has_many :posts, dependent: :nullify
+
   validates :name, presence: true
   validates :name, length: { maximum: 100, minimum: 2 }
   validates :name, uniqueness: true
